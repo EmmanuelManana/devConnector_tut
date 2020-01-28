59d993a971021d81c4edbec0b3d6dc3a4b97f4f6
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-01-28 16:56:38 +0200

    Get current userb profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-//@route    GET api/profile
-//@desc     profile route
-//@access   public   
-router.get('/', (req, res) => {
-    res.send('Profile Route')
-})
+//@route    GET api/profile/me
+//@desc     Get current users profile.
+//@access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    });
+
+    if (!profile) {
+      res.status(400).json({ msg: 'no profile for this user' });
+    } else {
+      profile.populate('user', [profile.user, 'avatar']);
+      res.json(profile);
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
